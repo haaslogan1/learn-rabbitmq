4035a091ac2f7299b127026ed43e7cf19e48a031
Logan Haas <lahaas@ncsu.edu>
2024-09-10 15:50:02 -0400

    Added durability to the queue and set the persistence property for the body of the message. This saves the message to disk to avoid losing messages. Must rename the queue to change properties (like durability).

--- WorkQueues/Worker/worker.cs
+++ WorkQueues/Worker/worker.cs
@@ -2,19 +2,29 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
+// Setting up the Connection Factory - Think SSL Connection Factory
 var factory = new ConnectionFactory { HostName = "localhost" };
+// Actually set up that connection - think port is open on 5671
 using var connection = factory.CreateConnection();
+// Create the channel to handle input recieved on the port that we just opened
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "hello",
-                     durable: false,
+// Declare the queue - whole reason why I am learning RabbitMQ ;)
+channel.QueueDeclare(queue: "task_queue",
+                     durable: true,
                      exclusive: false,
                      autoDelete: false,
                      arguments: null);
 
+// Inform the operator that no consumers have sent messages yet
 Console.WriteLine(" [*] Waiting for messages.");
 
+
+// Set up consumer object
 var consumer = new EventingBasicConsumer(channel);
+
+// Lambda function to iterate through each time the 'Received' event occurs for the consumer object
+// 
 consumer.Received += (model, ea) =>
 {
   var body = ea.Body.ToArray();
@@ -25,9 +35,12 @@ consumer.Received += (model, ea) =>
   Thread.Sleep(dots * 1000);
 
   Console.WriteLine(" [X] Done");
+
+  // here channel could also be accessed as ((EventingBasicConsumer)sender).Model
+  channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 };
 channel.BasicConsume(queue: "hello",
-                     autoAck: true,
+                     autoAck: false,
                      consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
